f7fdae5e4c5aa777dce5970a66fe5b07be3c2152
AshenChigo <chigoexploitkid@atomicmail.io>
2026-05-16 08:49:30 -0400

    Delete bypass.js

--- bypass.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){var destination = prompt("Destination (Don't include http/https: ");window.location='https://translate.google.com/translate?sl=auto&tl=en&u='+'https://'+destination})();
